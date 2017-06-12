0b90dce089350001c30e10787cfdea62d6d41b73
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-06-12 15:52:12 -0500

    more bones

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
 "use strict";
 
 const app = angular.module("TenderTummies", ["ngRoute"]);
+
+app.run((fbcreds)=>{
+   let cred = fbcreds;
+   let authConfig = {
+    apiKey: cred.apiKey,
+    authDomain: cred.authDomain,
+    databaseURL: cred.databaseUrl
+   };
+
+   firebase.initializeApp(authConfig);
+});
