bc664e6e03b17ba2e35262361753aef42888d7bb
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-06-13 21:04:47 -0500

    starting on safe

--- app/factories/SafeFactory.js
+++ app/factories/SafeFactory.js
@@ -2,4 +2,17 @@
 
 app.factory("SafeFactory", function($q, $http, fbcreds){
     
+    const addSafe = ( object ) => {
+    	return $q( (resolve, reject) => {
+    		let safeObj = JSON.stringify(object);
+    		$http.post(`${fbcreds.databaseURL}/safe.json`, safeObj)
+    		.then( response => {
+    			resolve(response);
+    		})
+    		.catch( error => {
+    			reject(error);
+    		});
+    	});
+    };
+
 });
