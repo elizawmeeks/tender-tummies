a8b4efdf3c252b486b19063f8f86a956f81498f5
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-06-21 11:11:31 -0500

    Got started on authentication, all in the userfactory for the moment

--- dist/app.js
+++ dist/app.js
@@ -1409,7 +1409,41 @@ app.factory("TriggerFactory", function($q, $http, fbcreds){
 });
 "use strict";
 
-app.factory("UserFactory", function($q, $http, fbcreds){
+app.factory("UserFactory", function($window){
+
+    //currentUser, createUser, loginUser, logoutUser, isAuthenticated getUser
+
+  let currentUser = null;
+
+  let logoutUser = function(){
+  		$window.location.href = '#!/splash';
+      return firebase.auth().signOut();
+  };
+
+  let isAuthenticated = function (){
+      return new Promise ( (resolve, reject) => {
+          firebase.auth().onAuthStateChanged( (user) => {
+              if (user){
+                  currentUser = user.uid;
+                  resolve(true);
+              }else {
+                  resolve(false);
+              }
+          });
+      });
+  };
+
+  let getUser = function(){
+      return currentUser;
+  };
+
+  let provider = new firebase.auth.GoogleAuthProvider();
+
+  let loginWithGoogle= function(){
+      return firebase.auth().signInWithPopup(provider);
+  };
+
+  return {logoutUser, isAuthenticated, getUser, loginWithGoogle};
     
 });
 "use strict";
